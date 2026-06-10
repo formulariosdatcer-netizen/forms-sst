7b51f7ef4529b3a7cb853fdc4aa59ac72a7d91a4
DATCER <ingenieriadatcer1@gmail.com>
2026-06-10 08:50:10 -0500

    Add Supabase credentials

--- js/config.js
+++ js/config.js
@@ -2,5 +2,5 @@
 // CONFIGURAR CON TUS CREDENCIALES DE SUPABASE
 // Ir a: https://supabase.com → Tu proyecto → Settings → API
 // ============================================================
-const SUPABASE_URL = '';
-const SUPABASE_ANON_KEY = '';
+const SUPABASE_URL = 'https://ahovwztoutpsdrzypzka.supabase.co';
+const SUPABASE_ANON_KEY = 'sb_publishable_KiElGfdVwHnI6Li4aRJN7g_WUfD_QDF';
